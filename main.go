a6834ed131a3d0f936f57dc6541d605617a04362
andrewfstratton <andrewfstratton@gmail.com>
2024-03-18 16:39:52 +0000

    Add Right stick and triggers

--- main.go
+++ main.go
@@ -32,14 +32,14 @@ var CustomGamepadHIDReport = []byte{
 	0x09, 0x32, //   Usage (Z) Right Stick
 	0x09, 0x33, //   Usage (Rx) Right Stick
 	0x09, 0x34, //   Usage (Ry) Left trigger
-	0x09, 0x35, //   Usage (Rz)
+	0x09, 0x35, //   Usage (Rz) Right trigger
 	0x75, 16, //   Report Size
 	0x95, 6, //   Report Count
 	0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
 	0xC0, // End Collection
 }
 
-func DefaultDefinitions() joystick.Definitions {
+func CustomDefinitions() joystick.Definitions {
 	return joystick.Definitions{
 		ReportID:     1,
 		ButtonCnt:    MAX_BTN_NUM,
@@ -58,7 +58,7 @@ func DefaultDefinitions() joystick.Definitions {
 func init() {
 	// Do this through flash ldflags options
 	// descriptor.Configure(0x2e8a, 0x0004)
-	joystick.UseSettings(DefaultDefinitions(), nil, nil, CustomGamepadHIDReport) // XboxHIDDescriptor)
+	joystick.UseSettings(CustomDefinitions(), nil, nil, CustomGamepadHIDReport) // XboxHIDDescriptor)
 }
 
 func main() {
@@ -84,23 +84,45 @@ func main() {
 					prefix := message[0:1]
 					message = strings.TrimPrefix(message, prefix)
 					switch prefix {
-					case "B": // button press
+					case "B", "b": // button press/release
 						btn_64, err := strconv.Atoi(message)
 						if err == nil {
-							gamepad.SetButton(int(btn_64), true)
+							press := false     // release by default
+							if prefix == "B" { // Button press
+								press = true
+							}
+							gamepad.SetButton(int(btn_64), press)
 							gamepad.SendState()
 						}
-					case "b": // button release
-						btn_64, err := strconv.Atoi(message)
+					case "X", "Y", "Z", "x", "y", "z": // Axis
+						val_64, err := strconv.Atoi(message)
 						if err == nil {
-							gamepad.SetButton(int(btn_64), false)
-							gamepad.SendState()
+							axis := -1 // In case it doesn't match
+							if -32767 <= val_64 && val_64 <= 32767 {
+								switch prefix {
+								case "X": // Left Stick < to >
+									axis = 0
+								case "Y": // Left Stick ^ to v
+									axis = 1
+								case "Z": // Right Stick < to >
+									axis = 2
+								case "x": // Right Stick ^ to v
+									axis = 3
+								case "y": // Left Trigger
+									axis = 4
+								case "z": // Right Trigger
+									axis = 5
+								}
+								if axis != -1 {
+									gamepad.SetAxis(axis, int(val_64))
+									gamepad.SendState()
+								}
+							}
 						}
 					}
 					message = ""
 				}
 			}
-			// gamepad.SetAxis(0, x)
 		}
 	}
 }
